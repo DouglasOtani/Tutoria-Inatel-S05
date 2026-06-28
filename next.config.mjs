d0d475fa043e83d4130363baad698f94b3b1ed3d
Douglas Otani <otanidouglas8@gmail.com>
2026-06-28 19:50:08 -0300

    chore: configure static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
+const repoName = "Tutoria-Inatel-S05";
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+	output: "export",
+	basePath: isProd ? `/${repoName}` : "",
+	assetPrefix: isProd ? `/${repoName}/` : "",
+	images: {
+		unoptimized: true,
+	},
+	env: {
+		NEXT_PUBLIC_BASE_PATH: isProd ? `/${repoName}` : "",
+	},
+};
 
-export default nextConfig
+export default nextConfig;
